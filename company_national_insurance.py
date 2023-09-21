da17160cace251556cb492860c258cb40652d4ff
Ben Taylor <ben.taylor2573@gmail.com>
2023-09-21 20:42:14 +0100

    company national insurance

--- company_national_insurance.py
+++ company_national_insurance.py
@@ -1,3 +1,11 @@
+from format_currency import cur
 
-def calc_company_national_insurance(gross: float) -> float:
-    return 0
+COMP_NAT_INS_THRESHOLD = 9_100
+COMP_NAT_INS_RATE = 0.138 # 13.8%
+
+def calc_comp_nat_ins(gross: float) -> float:
+    gross_over_threshold = max(0, gross - COMP_NAT_INS_THRESHOLD)
+    nat_ins = gross_over_threshold * COMP_NAT_INS_RATE
+
+    print(f"Company National Insurance: {cur(nat_ins)}")
+    return nat_ins
