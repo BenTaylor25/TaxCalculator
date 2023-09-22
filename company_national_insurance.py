a8ae199f7f04695e2b1935e325f2247ab19f7f60
Ben Taylor <ben.taylor2573@gmail.com>
2023-09-22 19:49:52 +0100

    fixed function name

--- company_national_insurance.py
+++ company_national_insurance.py
@@ -3,7 +3,7 @@ from format_currency import cur
 COMP_NAT_INS_THRESHOLD = 9_100
 COMP_NAT_INS_RATE = 0.138 # 13.8%
 
-def calc_comp_nat_ins(gross: float) -> float:
+def calc_company_national_insurance(gross: float) -> float:
     gross_over_threshold = max(0, gross - COMP_NAT_INS_THRESHOLD)
     nat_ins = gross_over_threshold * COMP_NAT_INS_RATE
 
